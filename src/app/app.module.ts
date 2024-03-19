151fba078f6001b9e1b43e8f5ba1cc0ee77d29d9
Ivan-Cecchi <i.cecchi@outlook.com>
2024-03-19 16:31:46 +0100

    Angular v1.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Components1Component } from './components1/components1/components1.component';
+import { Components2Component } from './components1/components2/components2.component';
+import { Components3Component } from './components1/components3/components3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Components1Component,
+    Components2Component,
+    Components3Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components1/components3/components3.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-components3',
+  templateUrl: './components3.component.html',
+  styleUrl: './components3.component.scss'
+})
+export class Components3Component {
+
+}
